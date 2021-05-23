4a73cc5c2d01eedc7f99df34a24a8e85cb805191
Tran Xuan Son <tranxuanson98@gmail.com>
2021-05-23 14:03:35 +0700

    config mySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'DESKTOP-66P8M1C',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'trello-clone',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) { }
+}
